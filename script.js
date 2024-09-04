f2858193c7fdb9be5a089fa0057d2647f31b2351
Alinatiurina <tiyrina.alina@gmail.com>
2024-09-04 16:57:43 +0300

    upgrade

--- script.js
+++ script.js
@@ -69,3 +69,46 @@ const swiper = new Swiper(".swiper", {
     el: ".swiper-pagination",
   },
 });
+const loadMoreBtn = document.querySelector(".load-more-btn");
+const isVisibled = document.querySelector(".hidden-section");
+const remoteBtn = document.querySelector(".remote-btn");
+
+function isVisible() {
+  setTimeout(() => {
+    isVisibled.classList.remove("isnt-visible");
+    loadMoreBtn.classList.add("isnt-visible");
+    remoteBtn.classList.remove("isnt-visible");
+  }, 100);
+}
+loadMoreBtn.addEventListener("click", isVisible);
+
+function isRemote() {
+  setTimeout(() => {
+    isVisibled.classList.add("isnt-visible");
+    loadMoreBtn.classList.remove("isnt-visible");
+    remoteBtn.classList.add("isnt-visible");
+  }, 100);
+}
+remoteBtn.addEventListener("click", isRemote);
+
+const loadMoreBtnHit = document.querySelector(".load-more-btn-hit");
+const isVisibledHit = document.querySelector(".hidden-section-hit");
+const remoteBtnHit = document.querySelector(".remote-btn-hit");
+
+function isVisibleHit() {
+  setTimeout(() => {
+    isVisibledHit.classList.remove("isnt-visible");
+    loadMoreBtnHit.classList.add("isnt-visible");
+    remoteBtnHit.classList.remove("isnt-visible");
+  }, 100);
+}
+loadMoreBtnHit.addEventListener("click", isVisibleHit);
+
+function isRemoteHit() {
+  setTimeout(() => {
+    isVisibledHit.classList.add("isnt-visible");
+    loadMoreBtnHit.classList.remove("isnt-visible");
+    remoteBtnHit.classList.add("isnt-visible");
+  }, 100);
+}
+remoteBtnHit.addEventListener("click", isRemoteHit);
